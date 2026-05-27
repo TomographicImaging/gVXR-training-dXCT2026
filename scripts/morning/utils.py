39b2220fcf932b2a173806443c1cad08395e6fd5
effepivi <franck.p.vidal@gmail.com>
2026-05-27 18:22:42 +0100

    Add a function to create the samples.

--- scripts/morning/utils.py
+++ scripts/morning/utils.py
@@ -17,8 +17,10 @@
 #   Authored by:    Franck Vidal (UKRI-STFC)
 #   Data by:    Matthew Jones (UCL)
 
-
+import os.path
 import numpy as np
+from gvxrPython3 import gvxr
+from tifffile import imread
 
 def pad_image(img, target_image_shape):
     """
@@ -98,3 +100,153 @@ def crop_image(img):
         cropped = np.copy(img)
 
     return  cropped
+
+def create_sample(sample_id: int,
+                  mesh_path: str):
+    """
+    Create a sample if needed
+
+    :param sample_id: the ID of the sample. Must be in the range [1, 6]
+    :param mesh_path: the path to the meshes
+    :return:
+    """
+
+    # Make sure the sample ID is in the range [1, 6]
+    assert(sample_id >= 1 and sample_id <= 6)
+
+    # Remove the sample if any
+    gvxr.removePolygonMeshesFromSceneGraph()
+
+    # Create an empty sample
+    gvxr.emptyMesh("sample")
+
+    # Load the image
+    labels = imread("../../data/morning/seg" + str(sample_id) + ".tif")
+    unit = "mm"
+
+    # Create the path if needed
+    if not os.path.exists(mesh_path):
+        os.makedirs(mesh_path)
+
+    # Crop the image
+    labels_cropped = crop_image(labels)
+
+    # Make sure it is a 3D image
+    if len(labels_cropped.shape) == 2:
+        labels = np.repeat(labels_cropped[np.newaxis, :, :], 5, axis=0)
+
+    # Set the pixel size
+    if sample_id == 1 or sample_id == 2 or sample_id == 3:
+        original_pixel_size = [0.01307412, 0.01307412]
+    else:
+        original_pixel_size = [0.01415419, 0.01415419]
+
+    material_composition = {
+
+        1: {
+            'name': "Al",
+            'material type': 'element',
+            'material': 'Al',
+            'density': 2.70
+        },
+
+        2: {
+            'name': "Cu-lower-density",
+            'material type': 'element',
+            'material': 'Cu',
+            'density': 1.87
+        },
+
+        3: {
+            'name': "NMC811",
+            'material type': 'mixture',
+            'material': 'NMC811',
+            'materials': ["Li", "Ni", "Mn", "Co"],
+            'weights': [0.8 * 0.1057, 0.8 * 0.8943, 0.1, 0.1],
+            'density': 2.61
+        },
+
+        4: {
+            'name': "Cu-higher-density",
+            'material type': 'element',
+            'material': 'Cu',
+            'density': 8.96
+        },
+
+        5: {
+            'name': "Fe70Cr19Ni10Mn1",
+            'material type': 'mixture',
+            'material': 'Fe70Cr19Ni10Mn1',
+            'density': 8.00
+        }
+    }
+
+    for label in np.unique(labels):
+        if label in material_composition.keys():
+            selected_material = material_composition[label]
+            mesh_label = material_composition[label]['name']
+
+            # Select the structure
+            binary_image = (labels == label).astype(np.uint8)
+
+            stl_fname = os.path.join(mesh_path, str(sample_id) + "-" + mesh_label + ".stl")
+
+            # Load the existing STL file
+            if os.path.exists(stl_fname):
+                gvxr.loadMeshFile(mesh_label, stl_fname, unit, False, "sample")
+            # Apply the Marching cubes
+            else:
+                print(f"Create {stl_fname}")
+
+                # Let' make it a cube
+                image_size = np.average([
+                    labels.shape[2] * original_pixel_size[0],
+                    labels.shape[1] * original_pixel_size[0]
+                ])
+
+                voxel_size = [
+                    original_pixel_size[0],
+                    original_pixel_size[1],
+                    image_size / labels.shape[0]
+                ]
+
+                # Create the mesh
+                gvxr.makeIsoSurface(mesh_label,
+                                    binary_image,
+                                    1,
+                                    0, 0, 0,
+                                    *voxel_size,
+                                    unit,
+                                    "sample"
+                )
+
+                # Save the mesh as an STL file
+                gvxr.saveSTLfile(mesh_label, stl_fname)
+
+            # Set the material
+            if selected_material['material type'].upper() == 'ELEMENT':
+                print("\tUse element", material_composition[label]["material"])
+                gvxr.setElement(mesh_label, material_composition[label]["material"])
+
+                if "density" in selected_material:
+                    gvxr.setDensity(mesh_label, selected_material["density"], "g/cm3")
+
+            elif selected_material['material type'].upper() == 'COMPOUND':
+                gvxr.setCompound(mesh_label, material_composition[label]["material"])
+                gvxr.setDensity(mesh_label, selected_material["density"], "g/cm3")
+
+            elif selected_material['material type'].upper() == 'MIXTURE':
+                if "materials" in selected_material and "weights" in selected_material:
+                    gvxr.setMixture(mesh_label, selected_material["materials"], selected_material["weights"])
+                else:
+                    gvxr.setMixture(mesh_label, material_composition[label]["material"])
+
+                gvxr.setDensity(mesh_label, selected_material["density"], "g/cm3")
+
+            else:
+                raise IOError("Invalid material type")
+
+            # Add the material
+            gvxr.addPolygonMeshAsInnerSurface(mesh_label)
+
+    return labels_cropped, original_pixel_size, unit
